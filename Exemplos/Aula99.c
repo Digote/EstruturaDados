b3cbe1877396565b2d5e572e458af28454160f93
DiegoTegani <45742711+DiegoTegani@users.noreply.github.com>
2022-11-14 15:35:10 -0300

    Estudando

---
+++ Exemplos/Aula99.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//                    Aula 99: Como criar um vetor?
+
+int main () {
+
+    int idade;
+    char sexo;
+    float n1;
+
+    int num1[10];
+    int num5[5] = {0};
+
+    char letras[100];
+    char vogais[5] = {'a','e','i','o','u'};
+
+    float notas[3] = {7.5,8.3,9.5};
+
+    return 0;
+}
