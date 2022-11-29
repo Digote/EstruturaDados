ace296133a6ac59098898e695f5ab7e80e4d4614
DiegoTegani <45742711+DiegoTegani@users.noreply.github.com>
2022-11-29 14:43:46 -0300

    atualização

--- Exercicios/ex003.c
+++ Exercicios/ex003.c
@@ -6,38 +6,29 @@ int main() {
     int vet1[2] = {1,2}, vet2[2] = {80,90};
     int i,j,c;
 
-    /* for(i = 0; i < 2; i++){
-        printf("Digite o valor da P %d do vetor1: ", i);
-        scanf("%d", &vet1[i]);
-    }
-    
-        for(j = 0; j < 2; j++){
-        printf("Digite o valor da P %d do vetor2: ", j);
-        scanf("%d", &vet2[j]);
-    }
-    */
-
     printf("\n");
 
     for(c = 0; c < 2; c++){
-        printf("Pocisao %d vetor1= %d ", c,vet1[c]);
-        printf("\n\n");
-        printf("Pocisao %d vetor2= %d ", c, vet2[c]);
+        printf("Pocisao %d vet1 = %d ", c,vet1[c]);
+        printf("\n");
+        printf("Pocisao %d vet2 = %d ", c, vet2[c]);
         printf("\n");
     }
+
+    c = 0;
     
-    for(i = 2; i > 2; i--){
+    for(i = 2; i >= 2; i--){
         for(j = 0; j < 2; j++)
-        vet1[i] = vet2[j];
+        vet1[i] =+ vet2[j];
     }
     
     printf("----------------------------------------------------");
     printf("\n");
     
     for(c = 0; c < 2; c++){
-        printf("Pocisao %d vetor1= %d ", c,vet1[c]);
-        printf("\n\n");
-        printf("Pocisao %d vetor2= %d ", c, vet2[c]);
+        printf("Pocisao %d vet1 = %d ", c,vet1[c]);
+        printf("\n");
+        printf("Pocisao %d vet2 = %d ", c, vet2[c]);
         printf("\n");
     }
 }
