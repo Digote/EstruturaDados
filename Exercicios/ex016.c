7f5b540c1a98d0ecaf0fb3142866bc61f1ac87fb
DiegoTegani <45742711+DiegoTegani@users.noreply.github.com>
2022-12-14 09:58:44 -0300

    +

---
+++ Exercicios/ex016.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+            Aula 126:   Faça um programa para jogar o jogo da velha.
+                        Ao final imprima o resultado do jogo e pergunta se deseja jogar novamente.
+*/
+
+
+int main() {
+
+    return 0; 
+}
