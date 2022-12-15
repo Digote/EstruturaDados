4037e02154631a8d43d5705b999227197c54f7fc
DiegoTegani <45742711+DiegoTegani@users.noreply.github.com>
2022-12-15 10:16:49 -0300

    +

---
+++ Exemplos/String/aula128.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+            Aula 128: O que é uma String?
+*/
+
+
+int main(){
+
+    char palavras[] = {"Oi. Vamos aprender a programar com a linguagem C?"};
+
+    printf("%s", palavras);
+
+    return 0;
+}
